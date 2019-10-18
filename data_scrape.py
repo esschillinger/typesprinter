db22d9063d62b16cb1f61ef5c494c0e420bf5c71
= <schillinger5810@students.d211.org>
2019-10-18 14:44:33 +0000

    changed capitalization of booleans

--- data_scrape.py
+++ data_scrape.py
@@ -6,17 +6,26 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.error import HTTPError
 
-def clean_text(text):
+def clean_text(text, isWiki):
     new = ''
+    bracket = False
     for char in text:
         ascii = ord(char)
         if ascii >= 32 and ascii <= 126:
-            new += char
+            if isWiki:
+                if ascii == 91:
+                    bracket = True
+                if not bracket:
+                    new += char
+                if ascii == 93: # Order matters here--place this after the character addition statement to ensure the right bracket is not included
+                    bracket = False
+            else:
+                new += char
 
     return new
 
-base_url = 'wikipedia.org'
-soup = BeautifulSoup(urlopen('https://www.' + base_url + '/'), 'html.parser')
+base_url = 'reddit.com'
+soup = BeautifulSoup(urlopen('https://www.' + base_url), 'html.parser')
 
 links = [a['href'] for a in soup.find_all('a', href=True) if len(a['href']) >= len(base_url) and not base_url in a['href']]
 print(links)
@@ -29,7 +38,7 @@ for i in range(0, len(links)):
     print(i, len(links), url)
     try:
         soup = BeautifulSoup(urlopen(url), 'html.parser')
-        data = [clean_text(p.text.strip()) for p in soup.find_all('p')]
+        data = [clean_text(p.text.strip(), 'wikipedia.org' in url) for p in soup.find_all('p')] # Evaluate whether or not it is a wikipedia link in-line
         texts.append(' '.join(data))
     except HTTPError:
         pass
