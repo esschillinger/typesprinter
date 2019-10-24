a59306886a855f0f7dcb7e4fe440ed458c852d5c
Ethan Schillinger <schillinger5810@students.d211.org>
2019-10-24 14:59:51 +0000

    set up gradient class for index.html header

--- static/canvas.js
+++ static/canvas.js
@@ -1,23 +1,20 @@
 function load_canvas() {
     var canvas = document.getElementById("canvas_terminal");
     var context = canvas.getContext("2d");
-
-    context.beginPath();
-    context.arc(20, 20, 10, 0, 2*Math.PI);
-    context.fillStyle = "#ff6159";
-    context.fill();
-
-    context.beginPath();
-    context.arc(50, 20, 10, 0, 2*Math.PI);
-    context.fillStyle = "#febf2d";
-    context.fill();
-
-    context.beginPath();
-    context.arc(80, 20, 10, 0, 2*Math.PI);
-    context.fillStyle = "#29cd42";
-    context.fill();
+    
+    drawCircle(20, 20, 10, "#ff6159", context);
+    drawCircle(50, 20, 10, "#febf2d", context);
+    drawCircle(80, 20, 10, "#29cd42", context);
     
     context.font = "25px Ubuntu Mono";
     context.fillStyle = "black";
     context.fillText("tph/bash", 450, 27.5);
+}
+
+//makes a circle of radius r with center {x, y} parameterized in the counterclockwise direction
+function drawCircle(x, y, r, color, context) {
+    context.beginPath();
+    context.arc(x, y, r, 0, 2 * Math.PI, true);
+    context.fillStyle = color;
+    context.fill();
 }
