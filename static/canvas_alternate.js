895ed346d6c54a2a25e146162a9aab8f41c1178e
Ethan Schillinger <schillinger5810@students.d211.org>
2019-11-15 00:25:06 +0000

    worked on canvas_alternate.js, it's a mess rn, maybe don't bother with it and instead revise canvas.js?

--- static/canvas_alternate.js
+++ static/canvas_alternate.js
@@ -4,6 +4,7 @@ let solid_cursor = true;
 let current_x = 65; // Use current_x and current_y to keep track of cursor position instead of character position
 let current_y = 40;
 let canvas_width = 1000;
+let canvas_height = 200;
 let character_width = 20;
 let space_width = 5;
 let cursor_width = 20;
@@ -41,7 +42,7 @@ function loadCanvas() {
     
     document.addEventListener('keydown', (e) => {
         if (acceptable_chars.includes(e.key.toLowerCase())) {
-            addCharacter(e.key, "white");
+            addCharacter(e.key);
         } else if (e.key === "Backspace") {
             removeCharacter();
         } else if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
@@ -50,10 +51,30 @@ function loadCanvas() {
     });
 }
 
+// Makes a circle of radius r with center {x, y} parameterized in the counterclockwise direction
+function drawCircle(x, y, r, color, context) {
+    context.beginPath();
+    context.arc(x, y, r, 0, 2 * Math.PI, true);
+    context.fillStyle = color;
+    context.fill();
+}
+
 function addCharacter(character) {
-    chars_locations.push([character, current_x, current_y]);
-    current_x += character_width + space_width;
-    if (current_x + character_width + space_width >= canvas_width) {
+    let index = checkCoordinates(current_x, current_y);
+    if (index == -1) {
+        chars_locations.splice(index, 0, [character, current_x, current_y]);
+        current_x += character_width + space_width;
+    } else {
+        for (var i = index; i < chars_locations.length; i++) {
+            chars_locations[i][1] += character_width + space_width;
+            if (chars_locations[i][1] > canvas_width - character_width - space_width) {
+                chars_locations[i][1] = 65;
+                chars_locations[i][2] += 40;
+            }
+        }
+    }
+  
+    if (current_x >= canvas_width) {
         current_x = 65;
         current_y += 40;
     }
@@ -62,42 +83,107 @@ function addCharacter(character) {
 }
 
 function removeCharacter() {
-    let index = checkCoordinates(current_x - space_width, current_y) // Must also account for if the previous character is the last character of the line above
-    for (var i = 0; i < chars_locations.length; i++) {
-        let set = chars_locations[i];
-        if (set[1] + space_width + character_width == current_x && set[2] == current_y) {
-            chars_locations.splice(i, 1); // Remove 1 element at position i
+    var index = -1;
+    let max_chars = Math.floor((canvas_width - 65) / character_width);
+    if (current_x == 65) {
+        index = checkCoordinates(65 + max_chars * (character_width + space_width), current_y - 40);
+    } else {
+        index = checkCoordinates(current_x - space_width - character_width, current_y); // Must also account for if the previous character is the last character of the line above
+    }
+    
+    if (index != -1) {
+        let removed = chars_locations.splice(index, 1);
+        current_x = removed[1];
+        current_y = removed[2];
+        
+        for (var i = index; i < chars_locations.length; i++) {
+            chars_locations[i][1] -= (character_width + space_width);
+            if (chars_locations[i][1] < 65) {
+                chars_locations[i][1] = 65 + max_chars * (character_width + space_width);
+                chars_locations[i][2] -= 40;
+            }
         }
+        console.log("(" + current_x + ", " + current_y + ")");
     }
   
     writeCanvas();
 }
 
-function moveCursor() {
-    current_x -= (character_width + space_width);
-    if (current_x < 65) {
-        current_x = 65;
-        current_y -= 40;
+function moveCursor(direction) {
+    if (direction == "Left") {
+        current_x -= (character_width + space_width);
+        if (current_x < 65) {
+            let max_chars = Math.floor((canvas_width - 65) / character_width);
+            current_x = 65 + max_chars * (character_width + space_width);
+            current_y -= 40;
+        }
+    } else {
+         current_x += character_width + space_width;
+         if (current_x > canvas_width) {
+             current_x = 65;
+             current_y += 40;
+         }
     }
     
     drawCursor();
 }
 
 function writeCanvas() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas_width, canvas_height);
+  
+    context.font = font;
+    context.fillStyle = "white";
+    context.fillText("$", 25, 40);
     
+    for (var i = 0; i < chars_locations.length; i++) {
+        let set = chars_locations[i];
+        context.fillText(set[0], set[1], set[2]);
+    }
+  
+    drawCursor();
 }
 
 function drawCursor() {
+    if (solid_cursor) {
+        context.fillStyle = "white";
+        context.fillRect(current_x, current_y - 33, cursor_width, cursor_height);
+    } else {
+        context.fillStyle = "black";
+        context.fillRect(current_x, current_y - 33, cursor_width, cursor_height);
+    }
     
+    if (chars_locations.length != 0) {
+        let last_char = chars_locations[chars_locations.length - 1];
+        if (!((current_x > last_char[1] && current_y == last_char[2]) || (current_x < last_char[1] && current_y > last_char[2]))) {
+            if (solid_cursor) {
+                context.fillStyle = "white";
+            } else {
+                context.fillStyle = "black";
+            }
+                context.font = font;
+                context.fillText(chars_locations[checkCoordinates(current_x, current_y)][0], current_x, current_y);
+        }
+    }
+  
+    solid_cursor = !solid_cursor;
 }
 
 function checkCoordinates(x, y) {
+    if (chars_locations.length == 0) {
+        return -1;
+    }
+    
     for (var i = 0; i < chars_locations.length; i++) {
         let set = chars_locations[i];
-        if (set[1] + space_width + character_width == x && set[2] == y) {
+        if (set[1] == x && set[2] == y) {
             return i;
         }
     }
   
     return -1;
+}
+
+function updateCursor() {
+    setInterval(function() { drawCursor(); }, 750);
 }
